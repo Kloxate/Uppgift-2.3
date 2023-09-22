d5f536eda5b55b56b3b101c924c9be2d50a45d81
Pingvu <nhlpool27@gmail.com>
2023-09-22 15:16:16 +0200

    Add project files.

---
+++ Uppgift 2.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace School_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej! Hur i hur många dagar vill du låna bilen?");
+            string Bilen = Console.ReadLine();
+            Console.WriteLine("Hur många kilometer ska du åka?");
+            string Kilometer = Console.ReadLine();
+            int Längd = int.Parse(Kilometer);
+            int Dagar = int.Parse(Bilen);
+            int hyrperdag = Dagar * 500;
+            int startavgift = + 300;
+            int Totalt = hyrperdag + startavgift + Längd;
+            Console.WriteLine("Ditt totala pris är, " + Totalt + "kr");
+        }
+    }
+}
